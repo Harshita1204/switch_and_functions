31adb7969efcc4b1dca9fe8d7ef8951d7511e4ff
Harshita1204 <harshitak1204@gmail.com>
2026-01-02 22:46:08 +0530

    Implement power function to calculate a^b

---
+++ parameterized_function_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+// return type , function name (arguments/ parameters)
+int power(int a, int b){
+    int ans=1;
+    for(int i=1;i<=b;i++){
+        ans=ans*a;
+    }
+    return ans;
+}
+int main() {
+  int a,b;
+  cout<<"Enter the value of a and b:"<<endl;
+  cin>>a;
+  cin>>b;
+  int answer=power(a,b);
+  cout<<"Answer is: "<<answer<<endl;
+  return 0;
+}
